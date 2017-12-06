c066e1b016de1c30adde21e81b68c78affba3434
JacobYun <jacob.yun429@gmail.com>
2017-12-05 19:10:37 -0800

    completed checkpoint, make sure to declare var before running get_me..

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,20 +1,26 @@
 
 require 'httparty'
+require 'json'
 
 class Kele
   include HTTParty
   base_uri 'https://www.bloc.io/api/v1'
 
   def initialize(email, password)
-    response = self.class.post('/sessions', body: { "email": email, "password": password })
-
-    if @auth == nil
-      'Invalid Credentials'
-    else
-      'Welcome'
-    end
+    response = self.class.post('/sessions', body: { email: email, password: password })
 
     @auth = response['auth_token']
+    if @auth == nil
+      puts 'Invalid Credentials'
+    else
+      puts 'Welcome'
+    end
 
   end
+
+  def get_me
+    response = self.class.get('/users/me', headers: { "authorization" => @auth })
+    JSON.parse(response.body)
+  end
+
 end
