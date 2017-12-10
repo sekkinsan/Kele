c2cdc749abd0eec76c3d9d470de10fcb09eb13c8
JacobYun <jacob.yun429@gmail.com>
2017-12-10 12:37:21 -0800

    can't find checkpoint id in get_me

--- lib/kele.rb
+++ lib/kele.rb
@@ -43,6 +43,16 @@ class Kele
     response = self.class.post("/messages", body: { "recipient_id": recipient_id, "subject": subject, "stripped-text": message}, headers: { "authorization" => @auth})
   end
 
+  def create_submission(enrollment_id, checkpoint_id, assignment_branch, assignment_commit_link, comment)
+    response = self.class.post("/checkpoint_submissions", body: { "enrollment_id": enrollment_id, "checkpoint_id": checkpoint_id, "assignment_branch": assignment_branch, "assignment_commit_link": assignment_commit_link, "comment": comment}, headers: { "authorization" => @auth})
+    if response["status"] == "submitted"
+      puts "Submission complete"
+    else
+      puts "Submission failed"
+    end
+  end
+
+
 
 
 end
