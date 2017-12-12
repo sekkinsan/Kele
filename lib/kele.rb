d794448bf088eb4bf43a9a15a609f308bf09081b
JacobYun <jacob.yun429@gmail.com>
2017-12-11 20:22:01 -0800

    edited order

--- lib/kele.rb
+++ lib/kele.rb
@@ -44,8 +44,8 @@ class Kele
     response = self.class.post("/messages", body: { "recipient_id": recipient_id, "subject": subject, "stripped-text": message}, headers: { "authorization" => @auth})
   end
 
-  def create_submission(enrollment_id, checkpoint_id, assignment_branch, assignment_commit_link, comment)
-    response = self.class.post("/checkpoint_submissions", body: { "enrollment_id": enrollment_id, "checkpoint_id": checkpoint_id, "assignment_branch": assignment_branch, "assignment_commit_link": assignment_commit_link, "comment": comment}, headers: { "authorization" => @auth})
+  def create_submission(checkpoint_id, enrollment_id, assignment_branch, assignment_commit_link, comment)
+    response = self.class.post("/checkpoint_submissions", body: { "checkpoint_id": checkpoint_id, "enrollment_id": enrollment_id, "assignment_branch": assignment_branch, "assignment_commit_link": assignment_commit_link, "comment": comment}, headers: { "authorization" => @auth})
     if response["status"] == "submitted"
       puts "Submission complete"
     else
